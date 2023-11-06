f07dea84c0e73fde9e47c207baff7f017a86861e
Chimi-S <chimithinley705@gmail.com>
2023-11-06 11:34:43 +0600

    Initial commit

---
+++ app.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <TrelloBoard/>
+  </div>
+</template>
---
+++ components/TrelloBoard.vue
@@ -0,0 +1,56 @@
+<template>
+    <div>
+        <div v-for="column in columns" :key="column.id">
+            <header>
+                {{ column.title }}
+            </header>
+            <p v-for="task in column.tasks" :key="task.id">
+                {{  task.title }}
+            </p>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { nanoid } from 'nanoid';
+import type { Column } from '../types/index.ts';
+
+const columns = ref<Column[]>([
+    {
+        id: nanoid(),
+        title: "Backlog",
+        tasks:[
+            {
+                id: nanoid(),
+                title: 'Create marking landing page',
+                createAt: new Date(),
+            },
+            {
+                id: nanoid(),
+                title: 'Develop cool new feature',
+                createAt: new Date(),
+            },
+            {
+                id: nanoid(),
+                title: 'Fix page nav bug',
+                createAt: new Date(),
+            }
+        ]
+        
+    },
+    {
+        id: nanoid(),
+        title: "Selected for Dev",
+        tasks:[]
+        
+    },
+    {
+        id: nanoid(),
+        title: "In Progress for Dev",
+        tasks:[]
+        
+    }
+])
+
+</script>
+
